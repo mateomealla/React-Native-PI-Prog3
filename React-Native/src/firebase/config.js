5e281af3af1fd9344a8142d87506fcca6fa71027
facuho <fho@udesa.edu.ar>
2025-11-06 21:20:07 -0300

    Configuración nuevo proyecto Firebase

--- React-Native/src/firebase/config.js
+++ React-Native/src/firebase/config.js
@@ -2,16 +2,18 @@ import app from "firebase/app";
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCrhGUUBLEITXTT2dvoH8GovkWEHblA9Zk",
-  authDomain: "my-proyect-a71d9.firebaseapp.com",
-  projectId: "my-proyect-a71d9",
-  storageBucket: "my-proyect-a71d9.firebasestorage.app",
-  messagingSenderId: "584354493451",
-  appId: "1:584354493451:web:b5f37fa8d8ab7c5853430e"
+  apiKey: "AIzaSyCKlWvZGzGsmPB9eWpEJqu7SPWyoNyS86Q",
+  authDomain: "proyectointegrador-6fc5a.firebaseapp.com",
+  projectId: "proyectointegrador-6fc5a",
+  storageBucket: "proyectointegrador-6fc5a.firebasestorage.app",
+  messagingSenderId: "1044324750089",
+  appId: "1:1044324750089:web:88b7dd1cab7a74452539d8"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
 export const db = firebase.firestore(); 
+export const storage = app.storage();
 
